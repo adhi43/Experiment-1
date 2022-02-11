37db38669fbc4b092287319edd2f108c1fc301c4
adhi43 <86366682+adhi43@users.noreply.github.com>
2022-02-11 09:42:16 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class addscan
+{
+     public static void main(String args[])
+     {
+     Scanner in=new Scanner (System.in);
+     System.out.println("Enter the two numbers:");
+     int x=in.nextInt();
+     int y=in.nextInt();
+     int c=0;
+     c=x+y;
+     System.out.println("Sum is ="+c);
+     }
+}
+
